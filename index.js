aec81289d5c6750a46e20f5f7a94ccfa284727b6
J2b2590 <J2b2590@gmail.com>
2020-11-03 07:57:38 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+console.log("HELLLOOo")
+
+const num1 = 1
+
+const num2 = 62
+
+const multiply = num1 * num2 
+
+const random = Math.floor( Math.random() * 1000 ) + 1
+
+const mod = 4 % 20
+
+const max = Math.max(1,2,4,4,5,6,20)
